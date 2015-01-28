125b91c2febba3265df62d2c9b07c311f2927e14
horaceheaven <hheaven@medullan.com>
2015-01-27 20:52:55 -0400

    Additional attributes for load

--- lib/meda/services/loader/profile_loader.rb
+++ lib/meda/services/loader/profile_loader.rb
@@ -6,7 +6,7 @@ module Meda
   #load the data store with profile data
   class ProfileLoader
 	
-   	def loadWithDefaultProfile(amount,config) 
+   	def loadWithDefaultProfile(amount, config) 
 
       profileStore = Meda::ProfileStore.new(config)
 
@@ -17,26 +17,38 @@ module Meda
     end
 
 
- 	def loadWithSomeProfileData(amount,config)
+ 	def loadWithSomeProfileData(amount, config)
 
       profileStore = Meda::ProfileStore.new(config)
   
 	 
       for i in 0..amount
-      	if( i % 100 == 0 )
-      		Meda.logger.info("--Loaded ------- #{i} profiles of #{amount}")
-      	end
-      	profileInfo = {}
-      	profileInfo[:cb] = UUIDTools::UUID.random_create.to_s
-      	profileInfo[:member_id] = UUIDTools::UUID.random_create.to_s
-    
-		profile = profileStore.find_or_create_profile(profileInfo)
-		profile_id = profile[:id]
+  
+    	if( i % 100 == 0 )
+    		Meda.logger.info("--Loaded ------- #{i} profiles of #{amount}")
+    	end
+      	
+      profileInfo = {}
+    	profileInfo[:cb] = UUIDTools::UUID.random_create.to_s
+    	profileInfo[:member_id] = UUIDTools::UUID.random_create.to_s
+  
+  		profile = profileStore.find_or_create_profile(profileInfo)
+  		profile_id = profile[:id]
 
-		additional_profile_info = {}
-		additional_profile_info[:age] = 21
-	  	additional_profile_info[:street] = 'piper'
-		profileStore.set_profile(profile_id,additional_profile_info)
+  		additional_profile_info = {}
+  		additional_profile_info[:age] = 21
+      additional_profile_info[:gender] = 'male'
+      additional_profile_info[:plan_option] = 'some plan'
+      additional_profile_info[:member_type] = 'member type'
+      additional_profile_info[:health_consumer] = 'some health_consumer'
+      additional_profile_info[:segmentation] = 'some segmentation'
+      additional_profile_info[:health_segmentation] = 'some health_segmentation'
+      additional_profile_info[:consumer_segmentation] = 'some consumer_segmentation'
+
+  		profileStore.set_profile(profile_id,additional_profile_info)
+
+      
+
 	  end
 
     end
