513cc962236ffce2eaa599cce5f79519367c6817
Wesley Boland <wesley@medullan.com>
2015-02-23 23:37:47 -0400

    prefixed logs

--- lib/meda/services/logging/logging_meta_data_service.rb
+++ lib/meda/services/logging/logging_meta_data_service.rb
@@ -22,40 +22,79 @@ module Meda
         hash["request_uuid"] = UUIDTools::UUID.random_create.to_s
         hash["request_url"] = @@request_url_filter_service.filter(request)
         hash["request_url_path"] = request.path_info
-        
-        hash = hash.merge(cookies)
-        hash = hash.merge(headers)
         hash["referrer"] = request.referrer
         hash["referer"] = request.referer
         hash["request_ip"] = Digest::SHA1.hexdigest(request_environment[:user_ip])
         hash["user_agent"] = request.user_agent
-        hash["cache_control"] = request.env['HTTP_CACHE_CONTROL']
-        hash = hash.merge(request.env['rack.request.query_hash']) if !request.env['rack.request.query_hash'].nil?
-        hash["hashed_member_id"] = @profile_id_service.stringToHash(hash["member_id"]) if !hash["member_id"].nil? && hash["member_id"].length > 0
-        hash["remote_address"] = Digest::SHA1.hexdigest(request.env['REMOTE_ADDR'].split(',').first)
+        hash["rack_cache_control"] = request.env['HTTP_CACHE_CONTROL']
+        hash["rack_remote_address"] = Digest::SHA1.hexdigest(request.env['REMOTE_ADDR'].split(',').first)
         
-        hash.delete("member_id")
-        hash.delete("COMPANY_ID")
-        hash.delete("ID")
-        hash.delete("PASSWORD")
-        hash.delete("LOGIN")
-        hash.delete("SCREEN_NAME")
-        hash.delete("fepblue#lang")
-        hash.delete("views")
-        hash.delete("SC_ANALYTICS_GLOBAL_COOKIE")
-        hash.delete("_ga")
-        hash.delete("_gat")
-        hash.delete("SC_ANALYTICS_SESSION_COOKIE")
-        hash.delete("BIPApp")
-        hash.delete("BIPCore")
-        hash.delete("starts")
-        hash.delete("alert-expires")
-        hash.delete("i18next")
+        cookies_hash = prefix_hash("cookie",cookies) 
+        hash = hash.merge(cookies_hash)
+
+        header_hash = prefix_hash("header",headers) 
+        hash = hash.merge(header_hash)
+        
+        query_string_hash = prefix_hash("querystring",request.env['rack.request.query_hash']) if !request.env['rack.request.query_hash'].nil?
+        hash = hash.merge(query_string_hash) if !query_string_hash.nil?
+
+        hash["hashed_member_id"] = @profile_id_service.stringToHash(hash["querystring_member_id"]) if !hash["querystring_member_id"].nil? && hash["querystring_member_id"].length > 0
+        
+        prefixes = []
+        prefixes.push("cookie")
+        prefixes.push("header")
+        prefixes.push("querystring")
+
+        hash = delete_with_prefixes(prefixes,"member_id",hash)
+        hash = delete_with_prefixes(prefixes,"COMPANY_ID",hash)
+        hash = delete_with_prefixes(prefixes,"ID",hash)
+        hash = delete_with_prefixes(prefixes,"PASSWORD",hash)
+        hash = delete_with_prefixes(prefixes,"LOGIN",hash)
+        hash = delete_with_prefixes(prefixes,"SCREEN_NAME",hash)
+        hash = delete_with_prefixes(prefixes,"fepblue#lang",hash)
+        hash = delete_with_prefixes(prefixes,"views",hash)
+        hash = delete_with_prefixes(prefixes,"SC_ANALYTICS_GLOBAL_COOKIE",hash)
+        hash = delete_with_prefixes(prefixes,"_ga",hash)
+        hash = delete_with_prefixes(prefixes,"_gat",hash)
+        hash = delete_with_prefixes(prefixes,"SC_ANALYTICS_SESSION_COOKIE",hash)
+        hash = delete_with_prefixes(prefixes,"BIPApp",hash)
+        hash = delete_with_prefixes(prefixes,"BIPCore",hash)
+        hash = delete_with_prefixes(prefixes,"starts",hash)
+        hash = delete_with_prefixes(prefixes,"alert-expires",hash)
+        hash = delete_with_prefixes(prefixes,"i18next",hash)
 
         Logging.mdc.clear
         Logging.mdc["meta_logs"] = hash.to_json
-  
-      end
+       
+    end
+
+
+    def prefix_hash(prefix,hash)
+        if hash.nil? 
+            return nil
+        end
+
+        prefix_hash = Hash.new()
+
+        hash.keys.each { |key| 
+            prefix_hash[prefix+'_'+key] = hash[key]
+        } 
+
+        prefix_hash
+    end
+
+    def delete_with_prefixes(prefixes, key, hash)
+        if hash.nil? 
+            return nil
+        end
+
+        prefixes.each { |prefix|
+            hash.delete(prefix+'_'+key)
+        }
+
+        hash
+
+    end
 
   end
 end
---
+++ spec/meda/services/logging/logging_meta_data_service_spec.rb
@@ -0,0 +1,105 @@
+require 'logging'
+
+describe Meda::LoggingMetaDataService do
+
+  describe 'logging meta data service' do
+    it 'prefixed a hash' do
+
+    	super_config = {}
+    
+      	config = Meda::Configuration.new();
+      	super_config["config"] = config
+
+      	logging_meta_data_service = Meda::LoggingMetaDataService.new(super_config)	
+      	
+      	prefix = "query"
+		hash =  Hash.new()
+		hash["one"] = "test1"
+      	hash["two"] = "test2"
+
+
+		result = logging_meta_data_service.prefix_hash(prefix,hash)
+
+		expect(result["query_one"]).to eql("test1")
+		expect(result["query_two"]).to eql("test2")
+		expect(result.keys.size).to eql(2)
+    end
+
+    it 'remove prefix key from hash' do
+
+    	super_config = {}
+      	config = Meda::Configuration.new();
+      	super_config["config"] = config
+
+      	logging_meta_data_service = Meda::LoggingMetaDataService.new(super_config)	
+      	
+      	prefixes = []
+      	prefixes.push("query")
+      	prefixes.push("cookie")
+      	prefixes.push("header")
+
+		hash =  Hash.new()
+		hash["query_one"] = "test1"
+      	hash["cookie_two"] = "test2"
+      	hash["header_two"] = "test3"
+      	hash["other_two"] = "test4"
+
+      	key = "two"
+		result = logging_meta_data_service.delete_with_prefixes(prefixes,key,hash)
+
+		expect(result["query_one"]).to eql("test1")
+		expect(result["cookie_two"]).to be_nil
+		expect(result["header_two"]).to be_nil
+		expect(result.keys.size).to eql(2)
+    end
+
+    it 'setup_meta_logs' do
+    	Logging.mdc.clear
+    	super_config = {}
+      	config = Meda::Configuration.new();
+      	super_config["config"] = config
+    	
+    	logging_meta_data_service = Meda::LoggingMetaDataService.new(super_config)
+
+		hash = Hash.new()
+		hash["rack.input"] = "rack.input"
+		hash["REQUEST_URI"] = "www.a.org/meda/identify.gif?dataset=ss&cb=4a262f24ff0c6847ed493d96f8f8c784e83d&member_id=13655045"
+		hash["rack.url_scheme"] = "http"
+		hash["SCRIPT_NAME"] = ""
+		hash["PATH_INFO"] = "www.a.org/meda/identify.gif"
+		hash["SERVER_PORT"] = "80"
+		hash["REQUEST_METHOD"] = "GET"
+		hash["QUERY_STRING"] = "dataset=ss&cb=4a262f24ff0c6847ed493d96f8f8c784e83d&member_id=13655045"
+		hash['CONTENT_LENGTH'] = "500"
+		hash['CONTENT_TYPE']= "application.json"
+		hash['REMOTE_ADDR'] = "aa,bb"
+
+    	request = Sinatra::Request.new(hash)
+    	headers = {}
+    	headers["test_header"] = "two"
+
+    	cookies = {}
+    	cookies["test_cookie"] = "one"
+
+    	request_environment = {}
+    	request_environment[:user_ip] = "foo"
+    	logging_meta_data_service.setup_meta_logs(request,headers,cookies,request_environment)
+
+    	logging_hash =  JSON.parse Logging.mdc["meta_logs"].to_s
+
+    	expect(logging_hash["rack_remote_address"]).to eql("e0c9035898dd52fc65c41454cec9c4d2611bfb37")
+    	expect(logging_hash["querystring_dataset"]).to eql("ss")
+    	expect(logging_hash["querystring_cb"]).to eql("4a262f24ff0c6847ed493d96f8f8c784e83d")
+    	expect(logging_hash["hashed_member_id"]).to eql("441e0a1e280099aacba724248b90ad574c069399")
+    	expect(logging_hash["request_url"]).to eql("http://www.a.org/meda/identify.gif?dataset=ss\u0026cb=4a262f24ff0c6847ed493d96f8f8c784e83d\u0026member_id__hashed=441e0a1e280099aacba724248b90ad574c069399")
+    	expect(logging_hash["request_url_path"]).to eql("www.a.org/meda/identify.gif")
+    	expect(logging_hash["request_ip"]).to eql("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33")
+    	expect(logging_hash["hashed_member_id"]).to eql("441e0a1e280099aacba724248b90ad574c069399")
+    	expect(logging_hash["header_test_header"]).to eql("two")
+    	expect(logging_hash["cookie_test_cookie"]).to eql("one")
+    end
+
+  end
+
+
+end
