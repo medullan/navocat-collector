3716f78bf2e2c1a045fe76548c4318bc5361ad1d
horaceheaven <hheaven@medullan.com>
2015-03-06 17:30:15 -0400

    Refactored validation to make valid_request use client_id

--- lib/meda/services/validation/validation_service.rb
+++ lib/meda/services/validation/validation_service.rb
@@ -9,23 +9,23 @@ module Meda
 
     end
 
-    def valid_request?(request_params)
-      if !request_params[:dataset].nil? && request_params[:dataset] != ''
-        Meda.logger.info("#{__method__} was called with dataset set to #{request_params[:dataset]}")
+    def valid_request?(client_id_cookie, request_params)
+      if !client_id_cookie.nil? && client_id_cookie != '' && !request_params[:dataset].nil? && request_params[:dataset] != ''
+        Meda.logger.info("#{__method__} was called with client_id: #{client_id_cookie} and dataset: #{request_params[:dataset]}")
         return true
       else
-        Meda.logger.error("#{__method__} was called with empty dataset, #{__method__} was called from #{__callee__}")
+        Meda.logger.error("#{__method__} was called with invalid client_id: #{client_id_cookie} or dataset: #{__method__} was called from #{__callee__}")
         return false
       end
     end
 
-    def valid_hit_request?(request_params)
-      if valid_request?(request_params)
-        if !request_params[:client_id].nil? && request_params[:client_id] != '' && !request_params[:path].nil? && request_params[:path] != ''
-          Meda.logger.info("#{__method__} was call with client_id: #{request_params[:client_id]} and path: #{request_params[:path]}")
+    def valid_hit_request?(client_id_cookie, request_params)
+      if valid_request?(client_id_cookie, request_params)
+        if !request_params[:path].nil? && request_params[:path] != ''
+          Meda.logger.info("#{__method__} was call with path: #{request_params[:path]}")
           return true
         else
-          Meda.logger.error("#{__method__} was called with invalid values, client_id: #{request_params[:client_id]}, path: #{request_params[:path]}")
+          Meda.logger.error("#{__method__} was called with invalid path: #{request_params[:path]}")
           return false
         end
       end
--- spec/meda/services/validation/validation_service_spec.rb
+++ spec/meda/services/validation/validation_service_spec.rb
@@ -5,6 +5,7 @@ describe 'Validation service' do
 
   before(:each) do
     @validation_service = Meda::ValidationService.new()
+    @client_id = "12345"
   end
 
   describe 'valid_request?' do
@@ -13,23 +14,37 @@ describe 'Validation service' do
       @dataset = { :dataset => "123456" }
     end
 
-    context 'with valid dataset' do
+    context 'with valid client_id and dataset' do
       it 'should return true' do
-        expect(@validation_service.valid_request?(@dataset)).to be_truthy
+        expect(@validation_service.valid_request?(@client_id, @dataset)).to be_truthy
       end
     end
 
     context 'with nil dataset'  do
       it 'should return false' do
         @dataset[:dataset] = nil
-        expect(@validation_service.valid_request?(@dataset)).to be_falsey
+        expect(@validation_service.valid_request?(@client_id,@dataset)).to be_falsey
       end
     end
 
     context 'with empty dataset' do
       it "should return false" do
         @dataset[:dataset] = ''
-        expect(@validation_service.valid_hit_request?(@dataset)).to be_falsey
+        expect(@validation_service.valid_hit_request?(@client_id, @dataset)).to be_falsey
+      end
+    end
+
+    context 'with nil client_id' do
+      it "should return false" do
+        @client_id = nil
+        expect(@validation_service.valid_hit_request?(@client_id, @dataset)).to be_falsey
+      end
+    end
+
+    context 'with empty client_id' do
+      it "should return false" do
+        @client_id = ''
+        expect(@validation_service.valid_hit_request?(@client_id, @dataset)).to be_falsey
       end
     end
 
@@ -38,40 +53,26 @@ describe 'Validation service' do
   describe 'valid_hit_request?' do
 
     before(:each) do
-      @request_hash = {:dataset => "123456", :client_id => "123456", :path => "/test/path"}
+      @request_hash = {:path => "/test/path"}
     end
 
-    context 'with valid client_id' do
+    context 'with valid path' do
       it "should return true" do
-        expect(@validation_service.valid_hit_request?(@request_hash)).to be_truthy
-      end
-    end
-
-    context 'with nil client_id' do
-      it 'should return false' do
-        @request_hash[:client_id] = nil
-        expect(@validation_service.valid_hit_request?(@request_hash)).to be_falsey
-      end
-    end
-
-    context 'with empty client_id' do
-      it 'should return false' do
-        @request_hash[:client_id] = ''
-        expect(@validation_service.valid_hit_request?(@request_hash)).to be_falsey
+        expect(@validation_service.valid_hit_request?(@client_id, @request_hash))
       end
     end
 
     context 'with nil path' do
       it 'should return false' do
         @request_hash[:path] = nil
-        expect(@validation_service.valid_hit_request?(@request_hash)).to be_falsey
+        expect(@validation_service.valid_hit_request?(@client_id, @request_hash)).to be_falsey
       end
     end
 
     context 'with nil path' do
       it 'should return false' do
         @request_hash[:path] = ''
-        expect(@validation_service.valid_hit_request?(@request_hash)).to be_falsey
+        expect(@validation_service.valid_hit_request?(@client_id, @request_hash)).to be_falsey
       end
     end
 
