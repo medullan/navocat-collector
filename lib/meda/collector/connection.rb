1445fdf2ed2826e32fc78de2a50619f6501bb86d
Sheldon Hall <sheldonhhall@gmail.com>
2015-03-17 13:58:40 -0400

    Fixed remove cb

--- lib/meda/collector/connection.rb
+++ lib/meda/collector/connection.rb
@@ -162,7 +162,7 @@ module Meda
 
       def process_request(params, &block)
         begin
-          params.delete(:cb)  #remove cb from all requests
+          params.delete('cb')  #remove cb from all requests
           dataset, other_params = extract_dataset_from_params(params)
           yield(dataset, other_params) if block_given?
 
