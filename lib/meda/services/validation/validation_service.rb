a8f8cf6a52526efa9cd18873df622098f31ad623
horaceheaven <hheaven@medullan.com>
2015-03-18 11:15:54 -0400

    updated validation methods to log at the debug level

--- lib/meda/services/validation/validation_service.rb
+++ lib/meda/services/validation/validation_service.rb
@@ -7,7 +7,7 @@ module Meda
 
     def valid_request?(client_id_cookie, request_params)
       if !client_id_cookie.blank? && !request_params[:dataset].blank?
-        Meda.logger.info("called with client_id: #{client_id_cookie} and dataset: #{request_params[:dataset]}")
+        Meda.logger.debug("called with client_id: #{client_id_cookie} and dataset: #{request_params[:dataset]}")
         return true
       else
         Meda.logger.error("called with invalid client_id: #{client_id_cookie} or dataset #{request_params[:dataset]}")
@@ -18,7 +18,7 @@ module Meda
     def valid_hit_request?(client_id_cookie, request_params)
       if valid_request?(client_id_cookie, request_params)
         if !request_params[:path].blank?
-          Meda.logger.info("call with path: #{request_params[:path]}")
+          Meda.logger.debug("call with path: #{request_params[:path]}")
           return true
         else
           Meda.logger.error("called with invalid path: #{request_params[:path]}")
@@ -31,7 +31,7 @@ module Meda
 
     def valid_profile_request?(client_id_cookie, request_params)
       if !request_params[:profile_id].blank?
-        Meda.logger.info("called with profile_id: #{request_params[:profile_id]}")
+        Meda.logger.debug("called with profile_id: #{request_params[:profile_id]}")
         return valid_request?(client_id_cookie, request_params)
       else
         Meda.logger.error("called with an empty profile_id: #{request_params[:profile_id]}")
